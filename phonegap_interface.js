85db242f63626127515050738de05b53e4998977
Fil Maj <filip.maj@nitobi.com>
2009-08-26 23:19:36 -0700

    Brought up issue with contact`s address field.

--- phonegap_interface.js
+++ phonegap_interface.js
@@ -13,5 +13,7 @@ function Contact() {
 		{'label':'mobile','value':''},
 		{'label':'home','value':''}
 	];
+	// Should address just be a string? BlackBerry, for example, stores specific sub-fields like street, city, country separately.
+	// Need to compare how iPhone, Android, BlackBerry and Windows Mobile each organize address fields.
 	this.address = "";
 }
