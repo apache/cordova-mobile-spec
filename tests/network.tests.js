51bc125f96a88399d601161dff14d4efb959daf4
Fil Maj <filip.maj@nitobi.com>
2009-12-01 14:50:07 -0800

    Bumped up test timeout to 15 seconds, added some Network (reachability) tests.

--- tests/network.tests.js
+++ tests/network.tests.js
@@ -9,4 +9,21 @@ Tests.prototype.NetworkTests = function() {
 		ok(navigator.network.isReachable != null, "navigator.network.isReachable should not be null.");
 		ok(typeof navigator.network.isReachable == 'function', "navigator.network.isReachable should be a function.");
 	});
+	test("should define constants for network status", function() {
+		expect(4);
+		ok(NetworkStatus != null, "NetworkStatus object exists in global scope.");
+		equals(NetworkStatus.NOT_REACHABLE, 0, "NetworkStatus.NOT_REACHABLE is equal to 0.");
+		equals(NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK, 1, "NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK is equal to 1.");
+		equals(NetworkStatus.REACHABLE_VIA_WIFI_NETWORK, 2, "NetworkStatus.REACHABLE_VIA_WIFI_NETWORK is equal to 2.");
+	});
+	test("isReachable function should return a NetworkStatus constant in its success callback", function() {
+		expect(1);
+		stop(Tests.TEST_TIMEOUT);
+		var hostname = "http://www.google.com";
+		var win = function(p) {
+			ok(p == NetworkStatus.NOT_REACHABLE || p == NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK || p == NetworkStatus.REACHABLE_VIA_WIFI_NETWORK, "Success callback in isReachable returns a proper NetworkStatus constant.");
+			start();
+		};
+		navigator.network.isReachable(hostname, win);
+	});
 };
