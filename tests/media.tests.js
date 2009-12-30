0640bd27e8792492aefa28b440161227e7078470
Fil Maj <filip.maj@nitobi.com>
2009-12-30 14:47:03 -0800

    Removed autoplay property from audio object.

--- tests/media.tests.js
+++ tests/media.tests.js
@@ -4,7 +4,15 @@ Tests.prototype.MediaTests = function() {
   		expect(1);
   		ok(typeof Audio == "function", "'Audio' should be defined as a function in global scope.");
 	});
-	test("should contain 'src', 'autoplay', 'loop' and 'error' properties", function() {
+	test("should define constants for Media errors", function() {
+		expect(5);
+		ok(MediaError != null && typeof MediaError != 'undefined', "MediaError object exists in global scope.");
+		equals(MediaError.MEDIA_ERR_ABORTED, 1, "MediaError.MEDIA_ERR_ABORTED is equal to 1.");
+		equals(MediaError.MEDIA_ERR_NETWORK, 2, "MediaError.MEDIA_ERR_NETWORK is equal to 2.");
+		equals(MediaError.MEDIA_ERR_DECODE, 3, "MediaError.MEDIA_ERR_DECODE is equal to 3.");
+		equals(MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED, 4, "MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED is equal to 4.");
+	});
+	test("should contain 'src', 'loop' and 'error' properties", function() {
   		expect(7);
 		var audioSrc = '/test.mp3';
 		var audio = new Audio(audioSrc);
@@ -15,13 +23,4 @@ Tests.prototype.MediaTests = function() {
 		ok(audio.loop == false, "Instantiated 'Audio' object's 'loop' property should initially be false.");
 		ok(typeof audio.error != 'undefined', "Instantiated 'Audio' object's 'error' property should not undefined.");
 		ok(audio.error == null, "Instantiated 'Audio' object's 'error' should initially be null.");
-	});
-	test("should define constants for Media errors", function() {
-		expect(5);
-		ok(MediaError != null && typeof MediaError != 'undefined', "MediaError object exists in global scope.");
-		equals(MediaError.MEDIA_ERR_ABORTED, 1, "MediaError.MEDIA_ERR_ABORTED is equal to 1.");
-		equals(MediaError.MEDIA_ERR_NETWORK, 2, "MediaError.MEDIA_ERR_NETWORK is equal to 2.");
-		equals(MediaError.MEDIA_ERR_DECODE, 3, "MediaError.MEDIA_ERR_DECODE is equal to 3.");
-		equals(MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED, 4, "MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED is equal to 4.");
-	});
 };
