fc77a47217e87533917830497c7fbfd4dce8a26a
Bryce Curtis <curtis.bryce@gmail.com>
2011-07-13 00:35:36 -0500

    Add phonegap.js as single place to edit when changing versions instead of each html file.

---
+++ phonegap.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-0.9.6.1.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="phonegap-0.9.6.1.js"></script>');
+document.write('<script>console.log("loading phonegap.js");</script>');
