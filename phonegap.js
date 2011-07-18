581987c03c3053ba0f79523bd8a496ed76bf524c
Bryce Curtis <curtis.bryce@gmail.com>
2011-07-18 15:03:20 -0500

    Add back button to navigate back to previous test page.

--- phonegap.js
+++ phonegap.js
@@ -1,3 +1,11 @@
 document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-0.9.6.1.js"></script>');
 document.write('<script type="text/javascript" charset="utf-8" src="phonegap-0.9.6.1.js"></script>');
-document.write('<script>console.log("loading phonegap.js");</script>');
+
+function backHome() {
+	if (device.platform.toLowerCase() == 'android') {
+		app.exitApp();
+	}
+	else {
+		document.location = "../index.html";
+	}
+}
