efbebaac75508f478017f3750b64283289cebc55
macdonst <simon.macdonald@gmail.com>
2011-12-14 17:41:13 -0500

    Fixing back button on Android
    If you click the back button on Android it calls navigator.app.exitApp() which was the old way of doing things. This will cause the app to close. Going back to setting the document location.

--- phonegap.js
+++ phonegap.js
@@ -2,10 +2,5 @@ document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-
 document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.2.0.js"></script>');
 
 function backHome() {
-	if (device.platform.toLowerCase() == 'android') {
-		navigator.app.exitApp();
-	}
-	else {
-		document.location = "../index.html";
-	}
+	document.location = "../index.html";
 }
