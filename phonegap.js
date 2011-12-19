5dbee6ca71e8dc5f4259bbbcc493e8941dec78d6
macdonst <simon.macdonald@gmail.com>
2011-12-19 10:46:34 -0500

    Adding battery tests to mobile spec

--- phonegap.js
+++ phonegap.js
@@ -1,5 +1,5 @@
-document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-1.2.0.js"></script>');
-document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.2.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-1.3.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.3.0.js"></script>');
 
 function backHome() {
 	if (device.platform.toLowerCase() == 'android') {
