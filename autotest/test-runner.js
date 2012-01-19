46ba34ee33c975afefa65853876531b3be4fb0fc
Fil Maj <filip.maj@nitobi.com>
2012-01-18 17:28:58 -0800

    tweaking init and ordering of tests

--- autotest/test-runner.js
+++ autotest/test-runner.js
@@ -7,23 +7,3 @@ sessionStorage.clear();
 // Android devices.
 var Tests = function() {};
 Tests.TEST_TIMEOUT = 2000;
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('deviceready', function() {
-        var tests = new Tests();
-
-        // Each group of tests are declared as a function in the object `Tests`.
-        // A group of tests are identified by a name that contains the word 'Tests'.
-        //
-        // Load each group of tests into QUnit
-        for (var t in tests) {
-            if (t.indexOf('Tests') > -1) {
-                tests[t]();
-            }
-        }
-        
-        // Start the QUnit test suite
-        QUnit.start();
-    }, false);
-}, false);
