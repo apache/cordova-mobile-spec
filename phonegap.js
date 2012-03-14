7de19632a08c4dba00eacf5b7cf9ec50f59a5f6e
Fil Maj <maj.fil@gmail.com>
2012-03-14 12:29:48 -0700

    tweaks to layout for new api specific test pages

--- phonegap.js
+++ phonegap.js
@@ -1,3 +1,4 @@
+document.write('<script type="text/javascript" charset="utf-8" src="../../phonegap-1.3.0.js"></script>');
 document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-1.3.0.js"></script>');
 document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.3.0.js"></script>');
 
