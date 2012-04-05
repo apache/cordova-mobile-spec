2ca11d82b9e7705be62d571099cdb7a7f0915d48
Jesse MacFadyen <purplecabbage@gmail.com>
2012-04-04 17:02:04 -0700

    fixed html errors, and various html tweaks, doctype, &nbsp; javascript:

--- phonegap.js
+++ phonegap.js
@@ -3,10 +3,11 @@ document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-
 document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.3.0.js"></script>');
 
 function backHome() {
-	if (device.platform.toLowerCase() == 'android') {
+	
+	if (window.device && device.platform && device.platform.toLowerCase() == 'android') {
             navigator.app.backHistory();
 	}
 	else {
-	    document.location = "../index.html";
+	    window.history.go(-1);
 	}
 }
