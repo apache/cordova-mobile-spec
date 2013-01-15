794d58346a4d72d3215ee3c32c51c70b56b7c1ef
Andrew Grieve <agrieve@chromium.org>
2013-01-15 14:12:02 -0500

    Add manual test for lazy-loading of cordova.js
    https://issues.apache.org/jira/browse/CB-1978

--- cordova.js
+++ cordova.js
@@ -23,7 +23,9 @@ var VERSION='2.3.0';
 var scripts = document.getElementsByTagName('script');
 var cordovaPath = scripts[scripts.length - 1].src.replace('cordova.js', 'cordova-'+VERSION+'.js');
 
-document.write('<script type="text/javascript" charset="utf-8" src="' + cordovaPath + '"></script>');
+if (!window._doNotWriteCordovaScript) {
+    document.write('<script type="text/javascript" charset="utf-8" src="' + cordovaPath + '"></script>');
+}
 
 function backHome() {
 	if (window.device && device.platform && device.platform.toLowerCase() == 'android') {
