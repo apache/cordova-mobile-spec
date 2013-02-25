224aaa4efc8212ee5ff63858858dc746af83acde
Michal Mocny <mmocny@gmail.com>
2013-02-25 15:59:04 -0500

    Support loading platform cordova.js files if available

--- cordova.js
+++ cordova.js
@@ -20,8 +20,25 @@
 */
 
 var VERSION='2.5.0';
+var PLAT = /Android/.exec(navigator.userAgent) ? 'android' : 'ios';
+
 var scripts = document.getElementsByTagName('script');
-var cordovaPath = scripts[scripts.length - 1].src.replace('cordova.js', 'cordova-'+VERSION+'.js');
+var currentPath = scripts[scripts.length - 1].src;
+var platformCordovaPath = currentPath.replace("cordova.js", "cordova." + PLAT + ".js");
+var versionCordovaPath = currentPath.replace("cordova.js", "cordova-" + VERSION + ".js");
+var cordovaPath;
+
+(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", platformCordovaPath, false);
+    xhr.onreadystatechange = function() {
+        if (this.readyState != this.DONE) {
+            return;
+        }
+        cordovaPath = (this.responseText.length > 0) ? platformCordovaPath : versionCordovaPath;
+    };
+    xhr.send(null);
+})();
 
 if (!window._doNotWriteCordovaScript) {
     document.write('<script type="text/javascript" charset="utf-8" src="' + cordovaPath + '"></script>');
