c00970f023917dd47cf31dd2aac3fba62023e748
timkim <timkim@apche.org>
2013-03-21 16:06:50 -0700

    [CB-2600] - fix for old blackberry

--- cordova.js
+++ cordova.js
@@ -41,13 +41,20 @@ if (PLAT) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", platformCordovaPath, false);
         xhr.onreadystatechange = function() {
+
             if (this.readyState == this.DONE && this.responseText.length > 0) {
-                cordovaPath = platformCordovaPath;
+                if(parseInt(this.status) >= 400){
+                    cordovaPath = versionCordovaPath;
+                }else{
+                    cordovaPath = platformCordovaPath;
+                }
             }
         };
         xhr.send(null);
     }
-    catch(e){} // access denied!
+    catch(e){
+        cordovaPath = versionCordovaPath;
+    } // access denied!
 }
 
 if (!window._doNotWriteCordovaScript) {
