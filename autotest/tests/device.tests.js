6839e1a6ea591b9573a478108d3753f5caf04784
ldeluca <ldeluca@us.ibm.com>
2013-05-23 13:44:21 -0400

    [CB-3485] Fixed device test to now check for model specification
    - added new Jasmine test

--- autotest/tests/device.tests.js
+++ autotest/tests/device.tests.js
@@ -52,4 +52,9 @@ describe('Device Information (window.device)', function () {
         expect(window.device.cordova).toBeDefined();
 		expect((new String(window.device.cordova)).length > 0).toBe(true);
 	});
+	
+	it("should contain a model specification that is a string", function() {
+        expect(window.device.model).toBeDefined();
+		expect((new String(window.device.model)).length > 0).toBe(true);
+	});
 });
