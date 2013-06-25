27b12450075570378da9f50e37bc642c9be64963
ldeluca <ldeluca@us.ibm.com>
2013-06-25 10:56:17 -0400

    [CB-3971] created corresponding mobile spec tests for separation of vibrations and notification
    - won't break existing platforms where code is not yet separated
    - breaks out the vibrations from the notifications so they may be tested
    separately.

--- autotest/tests/vibration.tests.js
+++ autotest/tests/vibration.tests.js
@@ -19,28 +19,13 @@
  *
 */
 
-describe('Notification (navigator.notification)', function () {
-	it("should exist", function() {
+describe('Vibration (navigator.notification.vibrate)', function () {
+	it("navigator.notification should exist", function() {
                 expect(navigator.notification).toBeDefined();
 	});
 
-	it("should contain a beep function", function() {
-		expect(typeof navigator.notification.beep).toBeDefined();
-		expect(typeof navigator.notification.beep).toBe("function");
-	});
-
-	it("should contain an alert function", function() {
-		expect(typeof navigator.notification.alert).toBeDefined();
-		expect(typeof navigator.notification.alert).toBe("function");
-	});
-
-	it("should contain a confirm function", function() {
-		expect(typeof navigator.notification.confirm).toBeDefined();
-		expect(typeof navigator.notification.confirm).toBe("function");
-	});
-	
-	it("should contain a prompt function", function() {
-		expect(typeof navigator.notification.prompt).toBeDefined();
-		expect(typeof navigator.notification.prompt).toBe("function");
+	it("should contain a vibrate function", function() {
+		expect(typeof navigator.notification.vibrate).toBeDefined();
+		expect(typeof navigator.notification.vibrate).toBe("function");
 	});
 });
