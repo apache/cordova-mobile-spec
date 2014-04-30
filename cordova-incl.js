a7c88d8fc6999996179356db7759db82782fee4e
Piotr Zalewa <piotr@zalewa.info>
2014-04-30 15:14:04 +0200

    Manual tests fixed
    Removing inline scripts from mobile-spec to make it compatible with FFOS priveleged type apps (i.e. using contacts)
    
    added addListenerToClass function which is used to connect buttons with test functions.
    
    Please check it on all platforms and r+ if working.
    
    moved test initialization to JS file (for DRY and privileged apps compatibility)

--- cordova-incl.js
+++ cordova-incl.js
@@ -56,6 +56,28 @@ if (!window._doNotWriteCordovaScript) {
     }
 }
 
+function addListenerToClass(className, listener, argsArray, action, doNotWrap) {
+    if (!action) {
+      action='click';
+    }
+    var elements = document.getElementsByClassName(className);
+    // choose Event target as a scope (like inline scripts)
+    if (!doNotWrap) {
+      if (argsArray && !Array.isArray(argsArray)) {
+        argsArray = [argsArray];
+      }
+      function callListener(e) {
+        listener.apply(null, argsArray);
+      }
+    } else {
+      callListener = listener;
+    }
+    for (var i = 0; i < elements.length; ++i) {
+      var item = elements[i];  
+      item.addEventListener(action, callListener, false);
+    }
+};
+
 function backHome() {
     if (window.device && device.platform && (device.platform.toLowerCase() == 'android' || device.platform.toLowerCase() == 'amazon-fireos')) {
         navigator.app.backHistory();
